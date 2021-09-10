4cc52a2018b9b2077ec9f76f6d49120424f51605
CandiceRee <rebeccaeechols@aol.com>
2021-09-09 23:25:36 -0500

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,11 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+//Assignment Code
+var generateBtn=document.querySelector("#generate");
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value=password;
 
-  passwordText.value = password;
-
+    //Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
